de6191244b2d00e051ca5004993db8a88f81d310
Mr Susumu Tsuchida <susumu.tsuchida@uqconnect.edu.au>
2020-01-17 14:31:27 +1000

    neat dynamics plots

--- code/dynamics/dynamics.py
+++ code/dynamics/dynamics.py
@@ -1,5 +1,6 @@
 from ..kernels.nn_kernel_elu import NNKernelElu
 from ..kernels.nn_kernel_gelu import NNKernelGelu
+from ..kernels.nn_kernel_relu import NNKernelRelu
 
 import matplotlib
 matplotlib.use('Agg')
@@ -19,20 +20,25 @@ import numpy as np
 import scipy.optimize as sci_opt
 from scipy.stats import multivariate_normal
 
-def sigma_star_plot(g):
+def sigma_star_plot(g, label, fig):
     norm_list = np.linspace(0.01, 8, 100)
     sigma_list = np.zeros_like(norm_list)
     for i, norm in enumerate(norm_list):
         root = sigma_star(g, norm)
         sigma_list[i] = root
-    plt.figure(figsize = (7,7))
-    plt.plot(norm_list, sigma_list)
+    if fig is None:
+        fig = plt.figure(figsize = (7,7))
+    else:
+        plt.figure(fig.number)
+    plt.plot(norm_list, sigma_list, label=label)
     plt.ylabel(r'$\sigma^*$', fontsize=40)
     plt.xlabel(r'$\Vert \mathbf{x} \Vert$', fontsize=40)
+    plt.legend(prop={'size':30})
     ax = plt.gca()
     ax.tick_params(axis = 'both', which = 'major', labelsize = 30)
     plt.savefig('code/dynamics/g_roots.pdf')
-    plt.close()
+
+    return fig
 
 def sigma_star(g, norm):
     """
@@ -56,7 +62,7 @@ def lambda_1_gelu(sigma, norm, theta):
     """
     # TODO: There is an issue with Williams' - arcsin can be undefined
     term1 = sigma**2/4*(1+2/np.pi*np.arcsin(\
-            (sigma*norm)**2*np.cos(theta)/(1+np.cos(theta)*sigma**2*norm**2)))
+            (sigma*norm)**2*np.cos(theta)/(1+sigma**2*norm**2)))
     term2 = sigma**4*norm**2*np.cos(theta)/\
             (np.pi*(norm**2*sigma**2+1)*\
             np.sqrt(sigma**4*norm**4*np.sin(theta)**2+2*norm**2*sigma**2+1))
@@ -82,43 +88,57 @@ def lambda_1_elu(sigma, norm, theta):
     
     return sigma**2*(term1+term2+term3)
 
-mode = 'elu'
-if mode == 'elu':
-    k_class = lambda std: NNKernelElu(1, std**2, 0, 0, 0, 1)
-elif mode == 'gelu':
-    k_class = lambda std: NNKernelGelu(1, std**2, 0, 0, 0, 1)
 
-k_fun = lambda std, norm: k_class(std).\
-        K(np.asarray([[norm]]), np.asarray([[norm]]))
-## NOTE: we divide by variance because output of k multiplies by another
-## variance for a linear output layer
-g = lambda std, norm: k_fun(std, norm)/(std**2) - norm**2
+# This code does two things:
+# (1) Draw sigma star against ||x||
+# (2) Plot the lambda_1 against theta for each activation function
+sigma_star_fig = None
+for mode in ['ELU', 'GELU', 'ReLU']:
+    if mode == 'ELU':
+        k_class = lambda std: NNKernelElu(1, std**2, 0, 0, 0, 1)
+    elif mode == 'GELU':
+        k_class = lambda std: NNKernelGelu(1, std**2, 0, 0, 0, 1)
+    elif mode == 'ReLU':
+        k_class = lambda std: NNKernelRelu(1, std**2, 0, 0, 0, 1)
 
-sigma_star_plot(g)
+    k_fun = lambda std, norm: k_class(std).\
+            K(np.asarray([[norm]]), np.asarray([[norm]]))
+    ## NOTE: we divide by variance because output of k multiplies by another
+    ## variance for a linear output layer
+    g = lambda std, norm: k_fun(std, norm)/(std**2) - norm**2
 
-norm_list = [0.1, 0.5, 1, 2, 5]
-theta_list = np.linspace(0.001, np.pi-0.001, 500)
-plt.figure(figsize=(7,7))
-for norm in norm_list:
-    # Find the value of sigma that preserves this norm
-    s = norm_preserving_s(g, norm)
-    sigma = s/norm
-    print(norm)
-    print(sigma)
+    # Plot sigma star
+    sigma_star_fig = sigma_star_plot(g, mode, sigma_star_fig)
 
-    # Now caluclate lambda_1 over the interval (0, pi)
-    lambda_list = np.zeros_like(theta_list)
-    for i, theta in enumerate(theta_list):
-        if mode == 'elu':
-            lambda_1 = lambda_1_elu(sigma, norm, theta)
-        elif mode == 'gelu':
-            lambda_1 = lambda_1_gelu(sigma, norm, theta)
-        lambda_list[i] = lambda_1
-    plt.plot(theta_list, lambda_list, label=str(norm))
-plt.ylabel(r'$\lambda_1$', fontsize=40)
-plt.xlabel(r'$\theta$', fontsize=40)
-ax = plt.gca()
-ax.tick_params(axis = 'both', which = 'major', labelsize = 30)
-plt.legend()
-plt.savefig('code/dynamics/lambda' + mode + '.pdf')
-plt.close()
+    # Plot lambda_1
+    norm_list = [0.1, 0.5, 1, 2, 5, 10, 20]
+    theta_list = np.linspace(0.001, np.pi-0.001, 500)
+    plt.figure(figsize=(7,7))
+    for norm in norm_list:
+        # Find the value of sigma that preserves this norm
+        s = norm_preserving_s(g, norm)
+        sigma = s/norm
+        print(mode)
+        print(norm)
+        print(sigma)
+
+        # Now caluclate lambda_1 over the interval (0, pi)
+        lambda_list = np.zeros_like(theta_list)
+        for i, theta in enumerate(theta_list):
+            if mode == 'ELU':
+                lambda_1 = lambda_1_elu(sigma, norm, theta)
+            elif mode == 'GELU':
+                lambda_1 = lambda_1_gelu(sigma, norm, theta)
+            elif mode == 'ReLU':
+                lambda_1 = (np.pi - theta)/np.pi
+            lambda_list[i] = lambda_1
+        plt.plot(theta_list, lambda_list, 
+                label=r'$\Vert \mathbf{x} \Vert = ' + str(norm) + '$')
+    plt.ylabel(r'$\lambda_1$', fontsize=40)
+    plt.xlabel(r'$\theta$', fontsize=40)
+    ax = plt.gca()
+    ax.tick_params(axis = 'both', which = 'major', labelsize = 30)
+    plt.legend(prop={'size':20})
+    plt.plot(theta_list, np.ones_like(theta_list), 'k--')
+    plt.savefig('code/dynamics/lambda_' + mode + '.pdf')
+    plt.close()
